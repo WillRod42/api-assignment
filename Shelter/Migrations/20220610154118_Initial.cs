ca09eb6904d236e3e85e773b34b1ce1392b9793c
William Rodriguez <william@rodriguez1.com>
2022-06-10 16:24:27 -0700

    Add database migrations

---
+++ Shelter/Migrations/20220610154118_Initial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiAssignment.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    AnimalTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.AnimalTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Breed = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    IsMale = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    TypeId = table.Column<int>(type: "int", nullable: false),
+                    AnimalTypeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                    table.ForeignKey(
+                        name: "FK_Animals_Types_AnimalTypeId",
+                        column: x => x.AnimalTypeId,
+                        principalTable: "Types",
+                        principalColumn: "AnimalTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_AnimalTypeId",
+                table: "Animals",
+                column: "AnimalTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+        }
+    }
+}
---
+++ Shelter/Migrations/20220610155521_SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiAssignment.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "AnimalTypeId", "Breed", "IsMale", "Name", "TypeId" },
+                values: new object[,]
+                {
+                    { 1, 3, null, "Beagle", true, "Fido", 1 },
+                    { 2, 1, null, "Calico", false, "Mittens", 2 },
+                    { 3, 13, null, "Parrot", true, "Fred", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Types",
+                columns: new[] { "AnimalTypeId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Dog" },
+                    { 2, "Cat" },
+                    { 3, "Bird" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Types",
+                keyColumn: "AnimalTypeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Types",
+                keyColumn: "AnimalTypeId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Types",
+                keyColumn: "AnimalTypeId",
+                keyValue: 3);
+        }
+    }
+}
